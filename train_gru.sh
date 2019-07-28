214d42ab895b09e84c63d8af0399b462b8cde059
Miguel Del Rio <drmiguel@mit.edu>
2019-07-28 12:56:44 -0500

    adding an update training file

--- train_gru.sh
+++ train_gru.sh
@@ -1,4 +1,6 @@
 for hidden in 100 90 80 70 60 50 40 30 20 10
 do
-	python3 gru_arithmetic.py --hidden_size $hidden --num_epochs 10
+	python3 gru_arithmetic.py --hidden_size $hidden --num_epochs 100
 done
+
+# sbatch --mem 32 --time 1000 run_training.sh

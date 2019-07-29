1bcb56f6ed03ad122bb45b92fecc04f152699005
Miguel A Del Rio <drmiguel@openmind7.cm.cluster>
2019-07-29 09:37:22 -0400

    adding the trained large models on fixed size inputs

--- bash_train.sh
+++ bash_train.sh
@@ -3,9 +3,9 @@
 SINGULARITY_IMG=/om2/user/jgauthie/singularity_images/deepo-cpu.simg
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd -P )"
 
-for hidden_value in 10 25 50 75 100
+for hidden_value in 100 75 50 25
 do
 	singularity exec -B /om/group/cpl -B "$PROJECT_PATH" "$SINGULARITY_IMG" python3 gru_arithmetic.py \
-		--num_epochs 100 --hidden_size $hidden_value \
+		--num_epochs 100 --hidden_size $hidden_value\
 		|| exit 1
 done
